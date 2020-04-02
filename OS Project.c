509c11ea1e1808625d1a7b12840b2a9e3f993c8c
Bhavesh Gujjula <51021251+bhaveshgujjula@users.noreply.github.com>
2020-04-02 12:18:06 +0530

    Also asking user input for max matrix and resources

--- OS Project.c
+++ OS Project.c
@@ -2,12 +2,12 @@
 int main() 
 { 
   
-    int n, m, i, j, k,alloc[15][15],max[15][15],avail[15];
+    	int n, m, i, j, k,alloc[15][15],max[15][15],avail[15];
 	printf("Enter the no of processes : ");
 	scanf("%d", &n);  
-    printf("Enter the no of resources : ");
+    	printf("Enter the no of resources : ");
 	scanf("%d", &m);
-    printf("\nEnter the Allocation Matrix for each process : ");
+	printf("\nEnter the Allocation Matrix for each process : ");
 	for(i = 0; i < n; i++)
 	{
 		printf("\nFor process %d : \n",i + 1);
@@ -16,5 +16,19 @@ int main()
 				scanf("%d", &alloc[i][j]);	
 			}
 	}
-   
+ 	printf("\n\nEnter the Maximum Matrix for each process : \n");
+	for(i = 0; i < n; i++)
+	{
+		printf("\nFor process %d : \n", i + 1);
+		for(j = 0; j < m; j++)
+		{
+			scanf("%d", &max[i][j]);
+		}
+	}
+ 
+  	printf("\n\nEnter the Available Resources : \n");
+	for(i = 0; i < m; i++)
+		{
+			scanf("%d", &avail[i]);
+		} 
 } 
